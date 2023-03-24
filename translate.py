ec6c63d282a50d46c7e9724168f421b42f22fc3b
Alar Suu <alar@thorgate.eu>
2023-03-24 19:53:20 +0200

    wip

--- translate.py
+++ translate.py
@@ -1,50 +1,73 @@
-"""
-Usage:
-
-poetry run python translate.py /path/to/your/django/project
-"""
-
 import os
-import re
-import sys
+from typing import Optional
 
-from constants import DEEPL_API_KEY, IGNORE_WORDS, PATTERN
+import libcst as cst
+
 from deepl_translate import translate
 
-if not DEEPL_API_KEY:
-    print("Please set DEEPL_API_KEY environment variable.")
-    sys.exit(1)
+
+class UserFacingStringMatcher(cst.CSTVisitor):
+    METADATA_DEPENDENCIES = (cst.MetadataWrapper,)
+
+    def __init__(self):
+        super().__init__()
+        self.matched_string = ""
+
+    def visit_Str(self, node):
+        print(node.value)
+        if self.get_metadata(self, node).is_argument:
+            # Ignore strings that are not arguments to functions
+            return False
+        if 'self.' in node.value:
+            # Ignore strings that reference attributes, model fields, methods, etc
+            return False
+        self.matched_string = node.value
 
 
-def translate_directory(directory_):
-    for root, dirs, files in os.walk(directory_):
-        if "migrations" not in root:
-            for file in files:
-                if file.endswith(".py"):
-                    filepath = os.path.join(root, file)
-                    # Open the file for reading and writing
-                    with open(filepath, "r+") as f:
-                        # Read the entire file into memory
-                        content = f.read()
-                        # Use regex to find all strings that need to be translated
-                        matches = re.findall(PATTERN, content)
-                        for match in matches:
-                            if match not in IGNORE_WORDS:
-                                # Call the translation function
-                                translated_text = translate(match)
-                                # Replace the original string with the translated string
-                                content = content.replace(
-                                    f'"{match}"', f'gettext("{translated_text}")'
-                                )
-                        # Return to the beginning of the file and write the modified content
-                        f.seek(0)
-                        f.write(content)
-                        f.truncate()
+class ReplaceString(cst.CSTTransformer):
+    def __init__(self, old_string: str, new_string: str):
+        super().__init__()
+        self.old_string = old_string
+        self.new_string = new_string
+
+    def leave_Call(self, original_node, updated_node):
+        visitor = UserFacingStringMatcher()
+        updated_node.visit(visitor)
+        if not visitor.matched_string:
+            return updated_node
+
+        if len(updated_node.args) > 0:
+            return cst.Call(func=(cst.Name(value='gettext')), args=[cst.Arg(cst.Str(updated_node.matched_string))])
 
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print(f"Usage: python {sys.argv[0]} <directory>")
-        sys.exit(1)
-    directory = sys.argv[1]
-    translate_directory(directory)
+def translate_file(file_path):
+    with open(file_path, 'r') as f:
+        source_code = f.read()
+
+    module = cst.parse_module(source_code)
+    matcher = UserFacingStringMatcher()
+    module.visit(matcher)
+
+    for node in matcher.matched_strings:
+        # Translate the string and wrap it in either `gettext()` or `gettext_lazy()`
+        translated_string = translate(node.value)
+        if 'models.' in file_path:
+            node.value = f'gettext_lazy("{translated_string}")'
+        else:
+            node.value = f'gettext("{translated_string}")'
+
+    modified_source_code = module.code
+    with open(file_path, 'w') as f:
+        f.write(modified_source_code)
+
+
+def translate_directory(directory_path):
+    for dir_path, _, filenames in os.walk(directory_path):
+        for filename in filenames:
+            if filename.endswith('.py'):
+                file_path = os.path.join(dir_path, filename)
+                translate_file(file_path)
+
+
+if __name__ == '__main__':
+    translate_directory('/home/alar/workspaces/thorgate/vestman-vaheladu/vaheladu/storage')
